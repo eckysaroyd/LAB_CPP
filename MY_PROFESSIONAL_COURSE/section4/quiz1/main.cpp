2ea65223f214b9c3954d02e77cf27d835d180d9f
eckysaroyd <enyato98@gmail.com>
2020-09-25 03:33:44 +0530

    new added quiz1

---
+++ MY_PROFESSIONAL_COURSE/section4/quiz1/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+
+int main ()
+{
+    int numb;
+    
+    std::cout<<"Enter your favourite number between 1 to 100"<<std::endl;
+    std::cin>>numb;
+    std::cout<<"That is my favourite number Too"<<std::endl;
+    std::cout<<"No really!  ";
+    std::cout<<numb;
+    std::cout<<"That is my favourite number too"<<std::endl;
+    return 0;
+}
