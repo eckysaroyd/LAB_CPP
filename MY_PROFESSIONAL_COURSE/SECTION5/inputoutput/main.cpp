c7b0b315df4999f7d4bb6e4b94599447bc3c1684
eckysaroyd <enyato98@gmail.com>
2020-09-29 05:34:58 +0530

    needed changes

---
+++ MY_PROFESSIONAL_COURSE/SECTION5/inputoutput/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+{
+    int a;
+    int b
+    
+  cout<<"hellow!! Enter two numbers separated with space"
+  cin>>a>>b
+  
+  cout <<"you have entered"<<a<<b;
+    
+    return 0;
+}
